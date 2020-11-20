d4580f2124812918e33c95ad37123b953b6432a3
陈杰 <jie.chen03@mihoyo.com>
2020-11-20 10:37:30 +0800

    add redis bitfilter

--- bitfilter/bitfilter_test.go
+++ bitfilter/bitfilter_test.go
@@ -1,28 +1,72 @@
 package bitfilter
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
 )
 
-func Test_filter(t *testing.T) {
-	filter := New(1024)
+func Test_MemoryBitFilter(t *testing.T) {
+	filter := NewMemoryBitFilter(1024)
 	data := make([][]byte, 2)
 	data[0] = []byte("12345")
 	data[1] = []byte("abcde")
-	filter.Add(data)
-	if !filter.IsAdd(data[0]) {
+	filter.Set(data[0])
+	filter.Set(data[1])
+	if filter.Check(data[0]) {
 		panic("data check error")
 	}
-	if !filter.IsAdd(data[1]) {
+	if filter.Check(data[1]) {
 		panic("data check error")
 	}
-	datas, addnum := filter.GetAllFilterData()
+	datas, addnum := filter.Export()
 	filter.Clear()
 	newfilter := Rebuild(datas, addnum)
-	if !newfilter.IsAdd(data[0]) {
+	if newfilter.Check(data[0]) {
 		panic("data check error")
 	}
-	if !newfilter.IsAdd(data[1]) {
+	if newfilter.Check(data[1]) {
 		panic("data check error")
+	}
+}
+func Test_RedisBitFilter(t *testing.T) {
+	pool := &redis.Pool{}
+	pool.Dial = func() (redis.Conn, error) {
+		return redis.Dial("tcp", "127.0.0.1:6379")
+	}
+	pool.MaxIdle = 10
+	pool.MaxActive = 10
+	pool.MaxConnLifetime = time.Hour
+
+	filter, e := NewRedisBitFilter(context.Background(), pool, "testbloom", &Option{})
+	if e != nil {
+		t.Fatal(e)
+	}
+	r, e := filter.Set(context.Background(), "test")
+	if e != nil {
+		t.Fatal(e)
+	}
+	if !r {
+		t.Fatal("add failed")
+	}
+	r, e = filter.Set(context.Background(), "test")
+	if e != nil {
+		t.Fatal(e)
+	}
+	if r {
+		t.Fatal("dup add failed")
+	}
+	rr, e := filter.Check(context.Background(), "test")
+	if e != nil {
+		t.Fatal(e)
+	}
+	if rr {
+		t.Fatal("check failed")
+	}
+	e = filter.clean()
+	if e != nil {
+		t.Fatal("clean error:" + e.Error())
 	}
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.13
 replace github.com/chenjie199234/Corelib => ../
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/golang/protobuf v1.4.2
+	github.com/gomodule/redigo/redis v0.0.0-20200429221454-e14091dffc1b
 	github.com/gorilla/websocket v1.4.2
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/shirou/gopsutil v2.20.9+incompatible
+	github.com/shirou/gopsutil v3.20.10+incompatible
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	google.golang.org/protobuf v1.25.0
 )
