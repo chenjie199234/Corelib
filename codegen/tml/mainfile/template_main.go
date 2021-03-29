269e94a37379da0c0402f315498e29862486bc8a
chenjie <nobodychen@lilith.com>
2021-03-29 14:21:53 +0800

    update

--- codegen/tml/mainfile/template_main.go
+++ codegen/tml/mainfile/template_main.go
@@ -81,6 +81,8 @@ func main() {
 	signal.Notify(ch, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	<-ch
 	close(stopreg)
+	//unregisterself
+	discoverysdk.UnRegisterSelf()
 	//stop the whole business service
 	service.StopService()
 	//stop low level net service
