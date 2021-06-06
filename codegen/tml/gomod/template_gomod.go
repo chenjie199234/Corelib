b725adf4866922db1da0ffcf89434f91d02d3630
chenjie199234 <chenjie199234@outlook.com>
2021-06-06 15:23:59 +0800

    Update template_gomod.go

--- codegen/tml/gomod/template_gomod.go
+++ codegen/tml/gomod/template_gomod.go
@@ -13,11 +13,11 @@ go 1.16
 require (
 	github.com/chenjie199234/Config v0.0.6
 	github.com/chenjie199234/Corelib v0.0.20
-	github.com/chenjie199234/Discovery v0.0.5
+	github.com/chenjie199234/Discovery v0.0.6
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/segmentio/kafka-go v0.4.14
-	go.mongodb.org/mongo-driver v1.5.1
+	github.com/segmentio/kafka-go v0.4.16
+	go.mongodb.org/mongo-driver v1.5.3
 	google.golang.org/protobuf v1.26.0
 )`
 
