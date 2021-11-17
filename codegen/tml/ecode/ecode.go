f55967cc915e82044cd35bc8d3564690a05624b0
chenjie <chenjie199234@outlook.com>
2021-11-17 22:52:27 +0800

    update

--- codegen/tml/ecode/ecode.go
+++ codegen/tml/ecode/ecode.go
@@ -14,13 +14,13 @@ import (
 
 var (
 	ErrUnknown  = cerror.ErrUnknown  //10000
-	ErrReq      = cerror.ErrReq      //10001
+	ErrReq      = cerror.ErrReq      //10001 // http code 400
 	ErrResp     = cerror.ErrResp     //10002
-	ErrSystem   = cerror.ErrSystem   //10003
-	ErrAuth     = cerror.ErrAuth     //10004
-	ErrLimit    = cerror.ErrLimit    //10005
-	ErrBan      = cerror.ErrBan      //10006
-	ErrNotExist = cerror.ErrNotExist //10007
+	ErrSystem   = cerror.ErrSystem   //10003 // http code 500
+	ErrAuth     = cerror.ErrAuth     //10004 // http code 401
+	ErrLimit    = cerror.ErrLimit    //10005 // http code 503
+	ErrBan      = cerror.ErrBan      //10006 // http code 403
+	ErrNotExist = cerror.ErrNotExist //10007 // http code 404
 
 	ErrBusiness1 = cerror.MakeError(20001, "business error 1")
 )`
