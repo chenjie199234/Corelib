967512ead2937326b89139d6e2ebf6c71b3136c6
chenjie <nobodychen@lilith.com>
2022-01-14 14:49:35 +0800

    update

--- proto.sh
+++ proto.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 protoc --go_out=paths=source_relative:. error/error.proto
 protoc --go_out=paths=source_relative:. crpc/msg.proto
 protoc --go_out=paths=source_relative:. pbex/pbex.proto
