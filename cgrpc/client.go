2a5612be5b1e65690df20986027949fadfa41511
nobody <lilithgames@lilithgamesdeMacBook-Pro-3.local>
2022-09-15 18:31:21 +0800

    update

--- cgrpc/client.go
+++ cgrpc/client.go
@@ -194,6 +194,11 @@ func (c *CGrpcClient) Call(ctx context.Context, path string, req interface{}, re
 		return e
 	}
 }
+
+func (c *CGrpcClient) Close() {
+	c.conn.Close()
+}
+
 func transGrpcError(e error) *cerror.Error {
 	if e == nil {
 		return nil
