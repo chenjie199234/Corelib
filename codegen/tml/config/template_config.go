4cba42172b60cad4ddaa36353d5fd950ae14f8d6
nobody <lilithgames@lilithgamesdeMacBook-Pro-4.local>
2022-10-24 23:30:36 +0800

    update

--- codegen/tml/config/template_config.go
+++ codegen/tml/config/template_config.go
@@ -140,12 +140,12 @@ import (
 type AppConfig struct {
 	HandlerTimeout         map[string]map[string]ctime.Duration $json:"handler_timeout"$ //first key handler path,second key method(GET,POST,PUT,PATCH,DELETE,CRPC,GRPC),value timeout
 	HandlerRate            []*publicmids.RateConfig             $json:"handler_rate"$
-	RateRedisUrl           string                               $json:"rate_redis_url"$    //if don't need global rate control,this can be empty
-	WebPathRewrite         map[string]map[string]string         $json:"web_path_rewrite"$  //first key method(GET,POST,PUT,PATCH,DELETE),second key origin url,value new url
-	Accesses               map[string]map[string]string         $json:"accesses"$          //first key-specific path,second key-accessid,value-accesskey,if first key-"default" means default accessid and accesskey
-	SessionRedisUrl        string                               $json:"session_redis_url"$ //if don't need session check,this can be empty
-	TokenSecret            string                               $json:"token_secret"$
-	SessionTokenExpire     ctime.Duration                       $json:"session_token_expire"$
+	RateRedisUrl           string                               $json:"rate_redis_url"$       //if don't need global rate control,this can be ignored
+	WebPathRewrite         map[string]map[string]string         $json:"web_path_rewrite"$     //first key method(GET,POST,PUT,PATCH,DELETE),second key origin url,value new url
+	Accesses               map[string]map[string]string         $json:"accesses"$             //first key-specific path,second key-accessid,value-accesskey,if first key-"default" means default accessid and accesskey
+	SessionRedisUrl        string                               $json:"session_redis_url"$    //if don't need session check,this can be ignored
+	TokenSecret            string                               $json:"token_secret"$         //if don't need token check,this can be ingored
+	SessionTokenExpire     ctime.Duration                       $json:"session_token_expire"$ //if don't need session and token check,this can be ignored
 	Service                *ServiceConfig                       $json:"service"$
 }
 type ServiceConfig struct {
@@ -154,7 +154,6 @@ type ServiceConfig struct {
 
 // every time update AppConfig will call this function
 func validateAppConfig(ac *AppConfig) {
-	os.Setenv("TOKEN_SECRET", ac.TokenSecret)
 }
 
 // AC -
