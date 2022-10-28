a0dd1548f750e010de98737e731228cc996e9e03
nobody <lilithgames@lilithgamesdeMacBook-Pro-4.local>
2022-10-28 10:07:48 +0800

    update

--- cgrpc/client.go
+++ cgrpc/client.go
@@ -159,7 +159,7 @@ func (c *CGrpcClient) ResolveNow() {
 	c.resolver.ResolveNow(resolver.ResolveNowOptions{})
 }
 func (c *CGrpcClient) Close() {
-	c.stop.Close(c.resolver.Close, func() { c.conn.Close() })
+	c.stop.Close(nil, func() { c.conn.Close() })
 }
 
 var ClientClosed = errors.New("[cgrpc.client] closed")
