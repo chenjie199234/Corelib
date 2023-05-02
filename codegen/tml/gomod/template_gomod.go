9957cc7b996a092c497cdc08884c7b4b2b95a5f3
nobody <chenjie199234@outlook.com>
2023-05-02 20:23:25 +0800

    update

--- codegen/tml/gomod/template_gomod.go
+++ codegen/tml/gomod/template_gomod.go
@@ -16,7 +16,7 @@ require (
 	github.com/chenjie199234/admin main
 	github.com/chenjie199234/Corelib %s
 	github.com/fsnotify/fsnotify v1.6.0
-	github.com/go-sql-driver/mysql v1.7.0
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/segmentio/kafka-go v0.4.39
 	go.mongodb.org/mongo-driver v1.11.4
 	google.golang.org/protobuf v1.30.0
