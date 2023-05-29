d32c0025967b9cbc37b0574bb74a062eb65be134
chenjie199234 <chenjie199234@gmail.com>
2023-05-29 10:29:37 +0800

    update

--- codegen/tml/gomod/template_gomod.go
+++ codegen/tml/gomod/template_gomod.go
@@ -17,7 +17,7 @@ require (
 	github.com/chenjie199234/Corelib %s
 	github.com/fsnotify/fsnotify v1.6.0
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/segmentio/kafka-go v0.4.39
+	github.com/segmentio/kafka-go v0.4.40
 	go.mongodb.org/mongo-driver v1.11.6
 	google.golang.org/protobuf v1.30.0
 )`
