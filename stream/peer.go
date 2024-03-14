b77d5fa6efcbd756fcf5b7a3bd812658c43cc8e7
chenjie199234 <chenjie199234@outlook.com>
2024-03-14 16:45:46 +0800

    update

--- stream/peer.go
+++ stream/peer.go
@@ -207,6 +207,8 @@ func (p *Peer) Close(block bool) {
 		<-p.blocknotice
 	}
 }
+
+// if uniqueid return in verify callback function is empty,the peer's RemoteAddr(ip:port) will be returned
 func (p *Peer) GetUniqueID() string {
 	if p.uniqueid == "" {
 		return p.GetRemoteAddr()
