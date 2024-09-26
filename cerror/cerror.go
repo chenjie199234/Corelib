280b5eafcbf89e82a98306f15e814fb95b8924a7
chenjie199234 <chenjie199234@outlook.com>
2024-09-26 16:50:17 +0800

    update

--- cerror/cerror.go
+++ cerror/cerror.go
@@ -11,6 +11,7 @@ import (
 	"github.com/chenjie199234/Corelib/util/common"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func MakeCError(code int32, httpcode int32, msg string) *Error {
@@ -82,7 +83,8 @@ func Decode(estr string) *Error {
 	if estr[0] == '{' && estr[len(estr)-1] == '}' {
 		//json format
 		tmp := &Error{}
-		if e := json.Unmarshal(common.STB(estr), tmp); e != nil {
+		//protojson can support "number string" or "number" for field:code
+		if e := (protojson.UnmarshalOptions{AllowPartial: true, DiscardUnknown: true}).Unmarshal(common.STB(estr), tmp); e != nil {
 			return MakeCError(-1, 500, estr)
 		}
 		if tmp.Code == 0 {
