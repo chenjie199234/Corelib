b447e383071b147827fc342f8e6684dc32601711
chenjie199234 <chenjie199234@outlook.com>
2025-02-21 13:37:26 +0800

    update

--- crpc/rw.go
+++ crpc/rw.go
@@ -107,10 +107,22 @@ func (this *rw) closerecvsend(trail bool, err error) error {
 	if old := atomic.AndInt32(&this.status, 0b1100); old&0b0011 == 0 {
 		return nil
 	}
-	fmt.Printf("%p", this)
+
+	fmt.Printf("%p\n", this)
 	thisaddr := atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&this)))
 	fmt.Println(thisaddr)
+
+	if this == nil {
+		fmt.Println("nil")
+	} else {
+		fmt.Printf("%p\n", this)
+		thisaddr := atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&this)))
+		fmt.Println(thisaddr)
+	}
 	this.e = err
+	fmt.Printf("%p\n", this)
+	thisaddr = atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&this)))
+	fmt.Println(thisaddr)
 	this.reader.Close()
 	m := &Msg{
 		H: &MsgHeader{
