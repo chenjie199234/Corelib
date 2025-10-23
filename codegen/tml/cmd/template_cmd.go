c498c98a5c288b36221e709587af21ceb24e3ead
chenjie199234 <chenjie199234@outlook.com>
2025-10-23 18:46:25 +0800

    update

--- codegen/tml/cmd/template_cmd.go
+++ codegen/tml/cmd/template_cmd.go
@@ -14,7 +14,7 @@ cd $(dirname $0)
 help() {
 	echo "cmd.sh — every thing you need"
 	echo "         please install git"
-	echo "         please install golang(1.24.1+)"
+	echo "         please install golang(1.25.0+)"
 	echo "         please install protoc           (github.com/protocolbuffers/protobuf)"
 	echo "         please install protoc-gen-go    (github.com/protocolbuffers/protobuf-go)"
 	echo "         please install codegen          (github.com/chenjie199234/Corelib)"
@@ -313,7 +313,7 @@ goto :eof
 :help
 	echo cmd.bat - every thing you need
 	echo           please install git
-	echo           please install golang(1.24.1+)
+	echo           please install golang(1.25.0+)
 	echo           please install protoc           (github.com/protocolbuffers/protobuf)
 	echo           please install protoc-gen-go    (github.com/protocolbuffers/protobuf-go)
 	echo           please install codegen          (github.com/chenjie199234/Corelib)
