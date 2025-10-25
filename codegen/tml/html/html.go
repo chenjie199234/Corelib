f292d05264dd0924bbef3643bbc44a6931ed388c
chenjie199234 <chenjie199234@outlook.com>
2025-10-25 22:39:49 +0800

    update

--- codegen/tml/html/html.go
+++ codegen/tml/html/html.go
@@ -82,6 +82,7 @@ const tsc = `{
 const vitec = `import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths()],
 })`
