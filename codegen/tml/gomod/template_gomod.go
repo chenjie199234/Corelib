d29ae69eb4ee845c508c6d59163d87bcde2429ea
chenjie199234 <chenjie199234@outlook.com>
2026-01-06 21:08:28 +0800

    update

--- codegen/tml/gomod/template_gomod.go
+++ codegen/tml/gomod/template_gomod.go
@@ -17,10 +17,10 @@ require (
 	github.com/chenjie199234/Corelib %s
 	github.com/fsnotify/fsnotify v1.9.0
 	github.com/go-sql-driver/mysql v1.9.3
-	github.com/redis/go-redis/v9 v9.16.0
-	go.mongodb.org/mongo-driver/v2 v2.4.0
-	google.golang.org/grpc v1.77.0
-	google.golang.org/protobuf v1.36.10
+	github.com/redis/go-redis/v9 v9.17.2
+	go.mongodb.org/mongo-driver/v2 v2.4.1
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.11
 )`
 
 func CreatePathAndFile(packagename string) {
